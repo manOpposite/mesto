e5efadef56d08e524d409c932b25a48ce1d76f22
Denis <badfingerdan@yandex.ru>
2020-12-04 21:38:28 +0300

    fixed 1 iter

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,14 @@
 
 let main = document.querySelector('.main');
 let editButton = main.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup'); 
-let closeButton = popup.querySelector('.popup__close-icon')
+let popup = document.querySelector('.popup');
+let closeButton = popup.querySelector('.popup__close-icon');
+let formElement = popup.querySelector('.popup__input-container');
+let jobInput = formElement.querySelector('.popup__item_job');
+let nameInput = formElement.querySelector('.popup__item_name');
+let profileName = main.querySelector('.profile__name');
+let profileJob = main.querySelector('.profile__text');
+let saveButton = popup.querySelector('.popup__save');
 
 
 function popupOpened() {
@@ -10,42 +16,28 @@ function popupOpened() {
   defaultData();
 }
 
+
 function popupClosed() {
   popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', popupOpened);
-closeButton.addEventListener('click', popupClosed);
-
-
-let formElement = popup.querySelector('.popup__input-container');
 
 function defaultData() {
-  let jobInput = formElement.querySelector('.popup__job');
-  let nameInput = formElement.querySelector('.popup__name');
-
-  let profileName = main.querySelector('.profile__name');
-  let profileJob = main.querySelector('.profile__text');
-
   jobInput.value = profileJob.textContent;
   nameInput.value = profileName.textContent;
 }
 
+
 function formSubmitHandler (evt) {
   evt.preventDefault();
 
-  let jobInput = formElement.querySelector('.popup__job');
-  let nameInput = formElement.querySelector('.popup__name');
-    
-  let profileName = main.querySelector('.profile__name');
-  let profileJob = main.querySelector('.profile__text');
-
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
+  
+  popupClosed();
 }
 
+editButton.addEventListener('click', popupOpened);
+closeButton.addEventListener('click', popupClosed);
 formElement.addEventListener('submit', formSubmitHandler);
-
-let saveButton = popup.querySelector('.popup__save');
 saveButton.addEventListener('click', formSubmitHandler);
-saveButton.addEventListener('click', popupClosed);
